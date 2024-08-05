ee872ea4085c3563ba7455434c066a4bfc0a6b5d
Ivson Santos <santosivson@hotmail.com>
2024-08-05 19:42:29 +0200

    Multiple classes

---
+++ src/main/java/com/example/demo/classes/MultipleClassesInSameFile.java
@@ -0,0 +1,23 @@
+package com.example.demo.classes;
+
+public class MultipleClassesInSameFile {
+
+    public static void main(String[] args) {
+
+        System.out.println(GenerateMessage.generateMessage());
+        System.out.println(AnotherMessage.generateAnotherMessage());
+    }
+
+}
+
+class GenerateMessage {
+    static String generateMessage() {
+        return "Here is one message";
+    }
+}
+
+class AnotherMessage {
+    static String generateAnotherMessage() {
+        return "Here is another message";
+    }
+}
